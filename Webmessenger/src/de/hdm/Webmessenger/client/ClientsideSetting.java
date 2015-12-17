ab9d6af07aedb85450649b64a92b01babfd11001
Alperen Guerev <alperen.guerev@hotmail.de>
2015-12-17 21:58:18 +0100

    Klasse ClientsideSetting hinzufügen.

---
+++ Webmessenger/src/de/hdm/Webmessenger/client/ClientsideSetting.java
@@ -0,0 +1,34 @@
+package de.hdm.Webmessenger.client;
+
+import java.util.logging.Logger;
+
+import de.hdm.Webmessenger.shared.*;
+import com.google.gwt.core.client.*;
+
+
+public class ClientsideSetting extends CommonSettings {
+
+	private static WebmessengerVerwaltungAsync webmessengerVerwaltung = null;
+	
+	private static final String LOGGER_NAME = "Webmessenger Web Client";
+	
+	private static final Logger log = Logger.getLogger(LOGGER_NAME);
+	
+	
+	public static Logger getLogger() {
+		return log;
+	}
+	
+	
+	
+	public static WebmessengerVerwaltungAsync getWebmessengerVerwaltung() {
+		if (webmessengerVerwaltung == null) {
+			webmessengerVerwaltung = (WebmessengerVerwaltungAsync) GWT.create(WebmessengerVerwaltung.class);
+			
+		}
+		return webmessengerVerwaltung;
+	}
+	
+	
+}
+
