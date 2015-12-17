2853c53fe31d93d34e2a5f48b5a98cd046ebe8e9
Arian Kurda <ak194@hdm-stuttgart.de>
2015-12-17 19:24:00 +0100

    Insert Methode in NutzerMapper implementieren.

--- Webmessenger/src/de/hdm/Webmessenger/server/db/DBConnection.java
+++ Webmessenger/src/de/hdm/Webmessenger/server/db/DBConnection.java
@@ -1,5 +1,38 @@
 package de.hdm.Webmessenger.server.db;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import com.google.appengine.api.utils.SystemProperty;
+
+public class DBConnection {
+	
+	private static Connection con = null;
+	
+	private static String = googleUrl = "";
+	private static String = localUrl = "";
+	
+	public static Connection connection() {
+		
+		if (con == null);
+			String url = null;
+			try {
+				if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production){
+					Class.forName("com.mysql.jdbc.GoogleDriver");
+					String googleUrl;
+					url = googleUrl;
+					
+				}else {
+					
+					Class.forName("com.mysql.jdbc.Driver");
+					String localUrl;
+					url = localUrl;
+				}
+				con = DriverManager.getConnection(url);
+			} catch (Exception e) {
+				con = null;
+				e.printStackTrace();
+			}
+			return con;
+	}
 }
