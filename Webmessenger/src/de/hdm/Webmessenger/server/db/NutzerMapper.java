e42d2f5b1f6066855aa8df81dda0c321824646ff
Arian Kurda <ak194@hdm-stuttgart.de>
2015-12-17 19:22:03 +0100

    Insert Methode in Klasse NutzerMapper implementieren.

--- Webmessenger/src/de/hdm/Webmessenger/server/db/NutzerMapper.java
+++ Webmessenger/src/de/hdm/Webmessenger/server/db/NutzerMapper.java
@@ -1,5 +1,52 @@
 package de.hdm.Webmessenger.server.db;
 
-public class NutzerMapper {
+import java.sql.*;
 
+import java.util.Vector;
+
+import de.hdm.Webmessenger.shared.bo.Nutzer;
+
+
+public class NutzerMapper {
+	
+private static NutzerMapper n = null;
+	
+	
+	protected NutzerMapper() {};
+	
+	
+	public static NutzerMapper nutzerMapper(){
+		if (n == null) {
+			n = new NutzerMapper();
+		}
+		return n;
+	}
+	
+	public Nutzer insert(Nutzer a){
+		Connection con = DBConnection.connection();
+		try {
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT MAX(NutzerId) AS maxid "
+          + "FROM nutzer");
+			
+			if(rs.next()){
+				a.setId(rs.getInt("maxid")+1);
+				
+				stmt = con.createStatement();
+				
+				stmt.executeUpdate("INSERT INTO nutzer (NutzerId, Email, Vorname, Nachname, Passwort)" + 
+				"VALUES (" + a.getId() + ",'" + a.getEmail() + "','" + a.getVorname() + "','" + a.getNachname() + "','" + a.getPassword() +"')");
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return a;
+	}
+	
+	
+	
 }
+	
+	
