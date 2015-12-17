958d6887c349340c6092d41db307d8da7beea578
Ahmet Özkeles <ao021@hdm-stuttgart.de>
2015-12-17 19:57:37 +0100

    Applikationslogik für Registrierung eines Nutzers implementieren. (Datenbankanbindungstest)

--- Webmessenger/src/de/hdm/Webmessenger/server/WebmessengerVerwaltungImpl.java
+++ Webmessenger/src/de/hdm/Webmessenger/server/WebmessengerVerwaltungImpl.java
@@ -1,9 +1,10 @@
 package de.hdm.Webmessenger.server;
 
 import de.hdm.Webmessenger.shared.*;
-import de.hdm.Webmessenger.shared.WebmessengerVerwaltung;
-
+import de.hdm.Webmessenger.shared.bo.Nutzer;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import de.hdm.Webmessenger.server.db.*;
+import com.google.gwt.user.client.Window;
 
 /**
  * Serverseitige Implementierung des Verwaltungsinterfaces.
@@ -13,6 +14,34 @@ public class WebmessengerVerwaltungImpl extends RemoteServiceServlet implements
 		WebmessengerVerwaltung {
 
 	
+private NutzerMapper nutzerMapper = null;
+	
+	
+	public WebmessengerVerwaltungImpl() throws IllegalArgumentException{};
+	
+	
+	public void init() throws IllegalArgumentException{
+		this.nutzerMapper = NutzerMapper.nutzerMapper();
+	}
+	
+	
+	public Nutzer nutzerErstellen(String vorname, String nachname, String email, String password)
+			throws IllegalArgumentException {
+		
+		Nutzer a = new Nutzer();
+		a.setVorname(vorname);
+		a.setNachname(nachname);
+		a.setEmail(email);
+		a.setPassword(password);
+		a.setId(1);
+		
+		
+		
+		return this.nutzerMapper.insert(a);
+		
+		
+		
+	}
 	
 	}
 
--- Webmessenger/src/de/hdm/Webmessenger/shared/bo/Nutzer.java
+++ Webmessenger/src/de/hdm/Webmessenger/shared/bo/Nutzer.java
@@ -4,4 +4,39 @@ public class Nutzer extends BusinessObject {
 	
 	public static final long serialVersionUID = 1L;
 
+
+	
+	private String vorname = null;
+	private String nachname = null;
+	private String email = null;
+	private String password = null;
+	
+	
+	
+	
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
